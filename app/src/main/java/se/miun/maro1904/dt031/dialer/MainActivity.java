cc1825778613ab8040c7e5c785e6183e2a9ba825
Marcus Roos <maro1904@student.miun.se>
2021-03-29 04:30:09 +0200

    Dial activity + button and function added.

--- app/src/main/java/se/miun/maro1904/dt031/dialer/MainActivity.java
+++ app/src/main/java/se/miun/maro1904/dt031/dialer/MainActivity.java
@@ -2,7 +2,9 @@ package se.miun.maro1904.dt031.dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dialButton(View view) {
+        Intent myIntent = new Intent(MainActivity.this, DialActivity.class);
+        MainActivity.this.startActivity(myIntent);
     }
 
 }
