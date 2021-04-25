8bd48d92f8423ba96ec199cda36597c260b8a806
Marcus Roos <maro1904@student.miun.se>
2021-04-26 00:26:33 +0200

    Working, need delete and call button.

--- app/src/main/java/se/miun/maro1904/dt031/dialer/DialActivity.java
+++ app/src/main/java/se/miun/maro1904/dt031/dialer/DialActivity.java
@@ -7,14 +7,19 @@ import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class DialActivity extends AppCompatActivity {
-    private DialpadButton button0, button1, button2, button3, button4, button5, button6, button7, button8,
+    private DialpadButton button2, button3, button4, button5, button6, button7, button8,
     button9, buttonStar, buttonPound;
+
 
     private TextView clicksTextView; // Display number of clicks on each button
     private String string = "";
 
+    List<DialpadButton> buttons = new ArrayList<DialpadButton>();
     // To count number of times each button changes color (is clicked)
     private int button1Clicks;
     private int button2Clicks;
@@ -23,33 +28,33 @@ public class DialActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
 
-        button0 = findViewById(R.id.dialpadbutton0);
-        button1 = findViewById(R.id.dialpadbutton1);
-        button2 = findViewById(R.id.dialpadbutton2);
-        button3 = findViewById(R.id.dialpadbutton3);
-        button4 = findViewById(R.id.dialpadbutton4);
-        button5 = findViewById(R.id.dialpadbutton5);
-        button6 = findViewById(R.id.dialpadbutton6);
-        button7 = findViewById(R.id.dialpadbutton7);
-        button8 = findViewById(R.id.dialpadbutton8);
-        button9 = findViewById(R.id.dialpadbutton9);
-        buttonStar = findViewById(R.id.dialpadbuttonstar);
-        buttonPound = findViewById(R.id.dialpadbuttonpound);
+        buttons.add(findViewById(R.id.dialpadbutton0));
+        buttons.add(findViewById(R.id.dialpadbutton1));
+        buttons.add(findViewById(R.id.dialpadbutton2));
+        buttons.add(findViewById(R.id.dialpadbutton3));
+        buttons.add(findViewById(R.id.dialpadbutton4));
+        buttons.add(findViewById(R.id.dialpadbutton5));
+        buttons.add(findViewById(R.id.dialpadbutton6));
+        buttons.add(findViewById(R.id.dialpadbutton7));
+        buttons.add(findViewById(R.id.dialpadbutton8));
+        buttons.add(findViewById(R.id.dialpadbutton9));
+        buttons.add(findViewById(R.id.dialpadbuttonstar));
+        buttons.add(findViewById(R.id.dialpadbuttonpound));
         clicksTextView = findViewById(R.id.numberArea);
 
 
-        button1.setOnClickedListener(new DialpadButton.OnClickedListener() {
-            @Override
-            public void onClick(DialpadButton me) {
-                String string = me.getTitle();
-                updateClicks(string);
-            }
-        });
+        for (int i=0; i<buttons.size(); i++) {
+            buttons.get(i).setOnClickedListener(me -> {
+                String aString = me.getTitle();
+                updateClicks(aString);
+            });
+        }
+
     }
 
     private void updateClicks(String aString) {
         string += aString;
-        clicksTextView.append(string);
+        clicksTextView.append(aString);
     }
 
 }
