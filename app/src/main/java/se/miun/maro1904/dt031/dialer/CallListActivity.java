2c0ed3ffedaf4aa34962a31db5d5110d8c578cf8
Marcus Roos <maro1904@student.miun.se>
2021-04-26 03:35:08 +0200

    Storing numbers work. Need to get settings option to work, and to delete.

--- app/src/main/java/se/miun/maro1904/dt031/dialer/CallListActivity.java
+++ app/src/main/java/se/miun/maro1904/dt031/dialer/CallListActivity.java
@@ -2,13 +2,25 @@ package se.miun.maro1904.dt031.dialer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CallListActivity extends AppCompatActivity {
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_call_list);
+        SharedPreferences sh = getSharedPreferences("MySharedPref", MODE_PRIVATE);
+        textView = findViewById(R.id.CalllistActivity);
+
+        String a = textView.getText().toString();
+        String s1 = sh.getString("name", "");
+        a += "\n" + s1;
+        textView.append(a);
     }
+
+
 }
--- app/src/main/java/se/miun/maro1904/dt031/dialer/DialActivity.java
+++ app/src/main/java/se/miun/maro1904/dt031/dialer/DialActivity.java
@@ -3,27 +3,53 @@ package se.miun.maro1904.dt031.dialer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 public class DialActivity extends AppCompatActivity implements View.OnClickListener, View.OnLongClickListener {
     private Button delButton, callButton;
-
-
     private TextView clicksTextView;
     Intent myIntent;
     List<DialpadButton> buttons = new ArrayList<>();
+
+    SharedPreferences sharedPreferences;
+    SharedPreferences.Editor myEdit;
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.settingsbutton) {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dial);
+        sharedPreferences = getSharedPreferences("MySharedPref",MODE_PRIVATE);
+        myEdit = sharedPreferences.edit();
         myIntent = new Intent(Intent.ACTION_DIAL);
         delButton = findViewById(R.id.DelButton);
         callButton = findViewById(R.id.CallButton);
@@ -50,6 +76,7 @@ public class DialActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
+
     private void updateClicks(String aString) {
         clicksTextView.append(aString);
     }
@@ -69,11 +96,16 @@ public class DialActivity extends AppCompatActivity implements View.OnClickListe
 
         callButton.setOnClickListener(newColor -> {
             String phoneNumber = "tel:" + clicksTextView.getText().toString();
-
+            String a = clicksTextView.getText().toString();
+            String s1 = sharedPreferences.getString("name", "");
+            a += "\n" + s1;
+            myEdit.putString("name", a);
+            myEdit.apply();
             if(phoneNumber.contains("#") || phoneNumber.contains("\u2733")){
                 phoneNumber = phoneNumber.replace("#","%23");
                 phoneNumber = phoneNumber.replace("\u2733","*");
             }
+
 
             Uri number = Uri.parse(phoneNumber);
             Intent callIntent = new Intent(Intent.ACTION_DIAL, number);
